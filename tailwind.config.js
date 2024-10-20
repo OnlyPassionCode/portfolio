7b159612651239b5f792260edcb74faf9952d629
OnlyPassionCode <van.wassenhove.kevin.pro@hotmail.com>
2024-10-20 21:17:55 +0200

    refactor nav + font instead of png for signature

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,12 @@ module.exports = {
   theme: {
     extend: {},
     screens: {
-      'xs': '530px',
-      'md': '840px',
+      'xs': '600px',
+      'sm':	'640px',
+      'md': '768px',
+      'lg':	'1024px',
+      'xl':	'1280px',
+      '2xl':	'1536px',
     }
   },
   plugins: [
